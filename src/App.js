44428685717ba32c3492193ca5f88682c89cfea9
nickubed <nhpquandt@gmail.com>
2021-01-12 15:11:13 -0800

    initial commit, jobs done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import DisplayCards from './displayCards'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  // Tha big standardz
+  let [data, setData] = useState({hits: []})
+  let [search, setSearch] = useState('')
+  // Stretch goal / bonus!
+  let [favs, setFavs] = useState([])
+
+  // Swipe em!
+  useEffect(() => {
+    console.log('hey')
+    fetch('http://acnhapi.com/v1/villagers/')
+    .then(response => response.json())
+    .then((rdata) => {
+      rdata = Object.values(rdata)
+      setData({hits: rdata})
+    })
+  }, [])
+
+  // Action for updating our search value
+  const handleChange = (e) => {
+    setSearch(e.currentTarget.value)
+  }
+
+  // Tailor our results
+  const dynamicSearch = () => {
+      return data.hits.filter(villager => villager.name['name-USen'].toLowerCase().includes(search.toLowerCase()))
+  }
+
+  // Build a "favorites" box
+  const handleClick = (villager) => {
+    if(!favs.includes(villager)){
+      setFavs([...favs, villager])
+    }
+  }
+
+  // add this bit to handle edge-case handling for DisplayCards bcuz we lazyyyyyyy
+  const handleDoofus = () => {
+    
+  }
+
+
+    return (
+      <div className="App">
+        <div className="searchBox">
+          <input type="text" value={search} onChange={handleChange} />
+        </div>
+        <div>
+          <DisplayCards handleClick={handleClick} data={dynamicSearch()}/>
+        </div>
+        <div className="favBox">
+          <DisplayCards handleClick={handleDoofus} data={favs} />
+        </div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/displayCards.jsx
@@ -0,0 +1,20 @@
+import react, {useState, useEffect} from 'react'
+
+const DisplayCards = (props) => {
+
+    return (
+        <div>
+            {props.data.map(villager => {
+                return (
+                <div className="villagerCard" onClick={() => {props.handleClick(villager)}}>
+                    <img className="villagerPic" src={villager['image_uri']} alt={villager.name['name-USen']} />
+                    <p>{villager.name['name-USen']}</p>
+                </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+export default DisplayCards
